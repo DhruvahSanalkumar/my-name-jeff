d78cf2be1a5414f323cd6e56980143168c2e37ed
DhruvahSanalkumar <spidervenom90r@gmail.com>
2026-02-12 08:42:10 +0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onSound(DetectedSound.Loud, function () {
     basic.showString(" DHRUVAH IS A SIGMA MEWING AURA MAN")
     basic.showLeds(`
-        . . . # .
-        # . # # #
+        . # . # .
+        # # # # #
         # # # # #
         . # # # .
         . . # . .
